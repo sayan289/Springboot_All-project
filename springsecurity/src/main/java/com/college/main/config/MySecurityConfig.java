b452378914331a4635ba55f78a94f920e2a9d210
sayan289 <142599454+sayan289@users.noreply.github.com>
2024-02-01 22:35:11 +0530

    Update MySecurityConfig.java

--- springsecurity/src/main/java/com/college/main/config/MySecurityConfig.java
+++ springsecurity/src/main/java/com/college/main/config/MySecurityConfig.java
@@ -9,29 +9,5 @@ import org.springframework.security.web.access.expression.WebSecurityExpressionR
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-        return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public UserDetailsService userDetailsService()
-    {
-        UserDetails normaluser=User.withUsername("sayan").password(passwordEncoder().encode("abc")).roles("NORMAL").build();
-        UserDetails adminuser=User.withUsername("subhajit").password(passwordEncoder().encode("abc")).roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(normaluser,adminuser);
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpsecurity) throws Exception {
-        httpsecurity.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/home/public").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin();
-        return httpsecurity.build();
-
-    }
+  
 }
